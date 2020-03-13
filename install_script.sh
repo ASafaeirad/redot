c4ff240623126c32f501b8d122e6a792acac5d76
frontendmonster <frontendmonster@gmail.com>
2020-03-14 01:39:40 +0330

    run script without root

--- install_script.sh
+++ install_script.sh
@@ -1,40 +1,31 @@
 #!/bin/bash
 set -e
 
-HOMEDIR=${1:-$HOME}
-
 echo "-------------"
-echo "$HOMEDIR"
+echo "$HOME"
 echo "-------------"
 
-if [[ "$EUID" -ne 0 ]]; then
-  echo "Sorry, this script must be ran as root"
-  echo "Maybe try this:"
-  echo "curl https://raw.githubusercontent.com/frontendmonster/redot/master/install_script.sh | sudo bash"
+if [[ ! -d $HOME ]]; then
+  echo "$HOME" is not exists.
   exit 1
 fi
 
-if [[ ! -d $HOMEDIR ]]; then
-  echo "$HOMEDIR" is not exists.
-  exit 1
-fi
+sudo apt update -y
+sudo apt install git tmux docker.io -y
 
-apt update -y
-apt install git tmux docker.io -y
-
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 
-BAKDIR="$HOMEDIR/redot-backup"
+BAKDIR="$HOME/redot-backup"
 [[ ! -d "$BAKDIR" ]] && mkdir "$BAKDIR"
-[[ -f "$HOMEDIR/.bashrc" ]] && mv "$HOMEDIR/.bashrc" "$BAKDIR/.bashrc"
+[[ -f "$HOME/.bashrc" ]] && mv "$HOME/.bashrc" "$BAKDIR/.bashrc"
 
-GITDIR="$HOMEDIR/.redot"
+GITDIR="$HOME/.redot"
 
 mkdir -p "$GITDIR"
 pushd "$GITDIR"
 
 git init --bare
-git --git-dir="$GITDIR" --work-tree="$HOMEDIR" remote add origin https://github.com/frontendmonster/redot.git
-git --git-dir="$GITDIR" --work-tree="$HOMEDIR" pull origin master -u
+git --git-dir="$GITDIR" --work-tree="$HOME" remote add origin https://github.com/frontendmonster/redot.git
+git --git-dir="$GITDIR" --work-tree="$HOME" pull origin master
