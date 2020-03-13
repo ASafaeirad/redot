b8cdd3b3d128eabe6cf86f4064d7caa141cc4ffe
frontendmonster <frontendmonster@gmail.com>
2020-03-13 21:01:21 +0330

    add install script

---
+++ install_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+HOMEDIR=${1:$HOME}
+
+if [[ "$EUID" -ne 0 ]]; then
+  echo "Sorry, this script must be ran as root"
+  echo "Maybe try this:"
+  echo "curl https://raw.githubusercontent.com/frontendmonster/redot/master/install_script.sh | sudo bash"
+  exit 1
+fi
+
+if [[ ! -d $HOMEDIR ]]; then
+  echo "$HOMEDIR" is not exists.
+  exit 1
+fi
+
+apt update -y
+apt install git tmux docker.io -y
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+BAKDIR="$HOMEDIR/redot-backup"
+[[ -f "$HOMEDIR/.bashrc" ]] && mv "$HOMEDIR/.bashrc" "$BAKDIR/.bashrc"
+
+GITDIR="$HOMEDIR/.redot"
+
+mkdir -p "$GITDIR"
+pushd "$GITDIR"
+
+git init --bare
+git --git-dir="$GITDIR" --work-tree="$HOMEDIR" remote add origin https://github.com/frontendmonster/redot.git
+git --git-dir="$GITDIR" --work-tree="$HOMEDIR" git pull origin master -u
