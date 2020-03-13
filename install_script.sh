5d820c9821104403a34525c0dba67cec8b4c49b5
frontendmonster <frontendmonster@gmail.com>
2020-03-14 01:26:38 +0330

    fix error on exiting docker-compose

--- install_script.sh
+++ install_script.sh
@@ -24,7 +24,7 @@ apt install git tmux docker.io -y
 
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 BAKDIR="$HOMEDIR/redot-backup"
 [[ ! -d "$BAKDIR" ]] && mkdir "$BAKDIR"
