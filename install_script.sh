2b85e3b62680648158d2dbe883193bcd2abe563c
frontendmonster <frontendmonster@gmail.com>
2020-04-03 13:58:56 +0430

    add docker groups

--- install_script.sh
+++ install_script.sh
@@ -16,6 +16,9 @@ sudo apt install git tmux docker.io -y
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo usermod -aG docker "$USER"
+sudo systemctl enable docker
+sudo systemctl start docker
 
 BAKDIR="$HOME/redot-backup"
 [[ ! -d "$BAKDIR" ]] && mkdir "$BAKDIR"
